ad49e3e6ee3fc7f8530e46142dc65c39dc11f32b
klawgolfer7 <kevinrlawler@gmail.com>
2016-06-08 21:27:09 -0400

    image carousel

--- js/main.js
+++ js/main.js
@@ -2,6 +2,23 @@ $(document).ready(function() {
 	// Get stellar going
 	$.stellar();
 
+	// Image Carousel click functions
+
+		$('.entry').on('click', function(e) {
+
+		var target = $(e.target)
+		var src = target.attr('src')
+
+		$('.lightbox img').attr('src', src);
+
+		$('body').addClass('lightbox-active');
+
+		$('.close').on('click', function() {
+			$('body').removeClass('lightbox-active');
+		});
+
+	});
+
 	// change the speed of clicking on the nav bar to get to the section
 	$('.nav-wrapper').onePageNav({
 				scrollSpeed: 1000
@@ -16,7 +33,7 @@ $(document).ready(function() {
 
 			});
 
-			// fix bug where menu is still hidden when we make our page bigger
+			// fix bug where menu is still hidden when I make the page bigger
 			$(window).resize(function () {
 				var w = $(window).width();
 
@@ -28,4 +45,17 @@ $(document).ready(function() {
 			});
 
 
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
